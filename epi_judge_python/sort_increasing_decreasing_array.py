566c9b03c3984ce689528f360fea5a926dfddf8f
Tahir Rauf <tahir.rauf1@gmail.com>
2019-02-21 21:10:22 -0800

    sort_increasing_decreasing_array

--- epi_judge_python/sort_increasing_decreasing_array.py
+++ epi_judge_python/sort_increasing_decreasing_array.py
@@ -1,9 +1,39 @@
 from test_framework import generic_test
+from heapq import heapify, heappop, heapreplace
 
 
 def sort_k_increasing_decreasing_array(A):
-    # TODO - you fill in here.
-    return []
+    increasing = 1
+    sorted_sub_arrays = []
+
+    start_idx = 0
+    for i in range(1, len(A)+1):
+        if i == len(A) \
+                or (A[i] < A[i-1] and increasing) \
+                or (A[i] >= A[i-1] and not increasing):
+            sorted_sub_arrays.append(A[start_idx:i] if increasing else A[i-1:start_idx-1:-1])
+            increasing ^= 1
+            start_idx = i
+
+    # merge all sorted arrays
+    return merge_sorted_arrays(sorted_sub_arrays)
+
+
+def merge_sorted_arrays(ssa):
+    res = []
+    h = [(lst[0], 0, lst_num) for lst_num, lst in enumerate(ssa) if lst]
+    heapify(h)
+
+    while len(h):
+        elem, elem_idx, lst_num = h[0]
+        # If this list has some more elements
+        if elem_idx+1 < len(ssa[lst_num]):
+            # Pop and replace the smallest item. Also push the new one. heap size does not change
+            heapreplace(h, (ssa[lst_num][elem_idx+1], elem_idx+1, lst_num))
+        else:
+            heappop(h)
+        res.append(elem)
+    return res
 
 
 if __name__ == '__main__':
